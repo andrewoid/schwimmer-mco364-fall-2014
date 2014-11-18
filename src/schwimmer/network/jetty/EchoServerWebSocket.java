00c8453093428328b40ed3d64e8e1c66d4f59326
Andrew <andrew@andrewoid.net>
2014-11-18 00:51:38 -0500

    added comments

--- src/schwimmer/network/jetty/EchoServerWebSocket.java
+++ src/schwimmer/network/jetty/EchoServerWebSocket.java
@@ -8,7 +8,9 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
+// http://www.eclipse.org/jetty/documentation/current/jetty-websocket-api-listener.html
 // http://www.eclipse.org/jetty/documentation/current/jetty-websocket-api-annotations.html
+// http://download.eclipse.org/jetty/stable-9/apidocs/org/eclipse/jetty/websocket/api/WebSocketListener.html
 @WebSocket(maxTextMessageSize = 64 * 1024)
 public class EchoServerWebSocket {
 	
